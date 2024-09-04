d37b17fbdc49540e03d74dc63edaf75a43a71b97
green <green@DESKTOP-012UDFQ>
2024-09-04 14:37:42 +0900

    20240904_1437 update

--- src/t50_ex/T5.java
+++ src/t50_ex/T5.java
@@ -2,6 +2,6 @@ package t50_ex;
 
 public class T5 {
 	public static void main(String[] args) {
-		System.out.println("안녕하하하하세요");
+		System.out.println("안녕하세요");
 	}
 }
--- src/t50_ex/T5Run.java
+++ src/t50_ex/T5Run.java
@@ -2,6 +2,6 @@ package t50_ex;
 
 public class T5Run {
 	public static void main(String[] args) {
-		System.out.println("안녕히히히히히히가세요.");
+		System.out.println("안녕히가세요.");
 	}
 }
