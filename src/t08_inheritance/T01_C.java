830f999fb86846e3e12b9966889ea7176be3daef
Jeonhyeunmin <jhm714@naver.com>
2024-09-05 20:09:36 +0900

    20240905_20:10

--- src/t08_inheritance/T01_C.java
+++ src/t08_inheritance/T01_C.java
@@ -5,5 +5,4 @@ public class T01_C extends T01_P{
 	void mod2() {
 		System.out.println("이곳은 T01_C 클래스의 mod2 메소드입니다.");
 	}
-	
 }
--- src/t50_ex/BankApplication.java
+++ src/t50_ex/BankApplication.java
@@ -5,43 +5,34 @@ import java.util.Scanner;
 public class BankApplication {
 	private static Ex20_Account accountArray[] = new Ex20_Account[100];
 	private static Scanner scanner = new Scanner(System.in);
-	
-	
+
 	public static void main(String[] args) {
 		boolean run = true;
-		while(run) {
+		while (run) {
 			System.out.println("-------------------------------------------------------");
 			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.계좌삭제 | 6. 종료");
 			System.out.println("-------------------------------------------------------");
 			System.out.print("선택> ");
-			
 			int selectNo = scanner.nextInt();
-			
-			if(selectNo == 1) {
+
+			if (selectNo == 1) {
 				createAccount();
-			}
-			else if(selectNo == 2) {
+			} else if (selectNo == 2) {
 				accountList();
-			}
-			else if(selectNo == 3) {
+			} else if (selectNo == 3) {
 				deposit();
-			}
-			else if(selectNo == 4) {
+			} else if (selectNo == 4) {
 				withdraw();
-			}
-			else if(selectNo == 5) {
+			} else if (selectNo == 5) {
 				delete();
-			}
-			else if(selectNo == 6) {
+			} else if (selectNo == 6) {
 				run = false;
 			}
 		}
 		System.out.println("프로그램 종료");
 	}
-	
 
-	//계좌생성하기
-	private static void createAccount() {
+	private static void createAccount() { // 계좌생성하기
 		System.out.println("------");
 		System.out.println("계좌생성");
 		System.out.println("------");
@@ -51,107 +42,94 @@ public class BankApplication {
 		String onwer = scanner.next();
 		System.out.print("초기입금액: ");
 		int balance = scanner.nextInt();
-		for(int i = 0; i < accountArray.length; i++) {
-			if(accountArray[i] == null) {
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] == null) {
 				accountArray[i] = new Ex20_Account(ano, onwer, balance);
-				System.out.print("결과 : 계좌가 생성되었습니다.");
+				System.out.println("결과 : 계좌가 생성되었습니다.");
 				break;
 			}
 		}
 	}
-	
-	//계좌목록보기
-	private static void accountList() {
+
+	private static void accountList() {	// 계좌목록보기
 		System.out.println("------");
 		System.out.println("계좌목록");
 		System.out.println("------");
-		
-		for(int i = 0; i < accountArray.length; i++) {
-			Ex20_Account account =  accountArray[i];
-			if(account != null) {
+
+		for (int i = 0; i < accountArray.length; i++) {
+			Ex20_Account account = accountArray[i];
+			if (account != null) {
 				System.out.print(account.getAno() + "\t");
 				System.out.print(account.getOnwer() + "\t");
 				System.out.print(account.getBalance() + "\n");
-			}
-			else{
+			} else {
+				System.out.println(" ");
 				break;
 			}
 		}
 	}
-	
-	//예금하기
-	private static void deposit() {
+
+	private static void deposit() {	// 예금하기
 		System.out.println("---");
 		System.out.println("예금");
 		System.out.println("---");
 		System.out.print("계좌번호: ");
 		String ano = scanner.next();
-		
 		System.out.print("예금액: ");
 		int money = scanner.nextInt();
-		
-	// 계좌 검색 메소드 호출
-		Ex20_Account account = findAccount(ano);
-		
-		if(findAccount(ano) == null) {
+		Ex20_Account account = findAccount(ano);			// 계좌 검색 메소드 호출
+		if (findAccount(ano) == null) {
 			System.out.println("결과: 찾는 계좌정보가 없습니다.");
 			return;
 		}
-		
-		//get으로 받아오는 금액에 입력한 money를 더해서 set에 넣음
-		account.setBalance(account.getBalance() + money);
+		account.setBalance(account.getBalance() + money);			// get으로 받아오는 금액에 입력한 money를 더해서 set에 넣음
 		System.out.println("결과 : 예금이 성공되었습니다.");
 	}
-	
-	//출금하기
-	private static void withdraw() {
+
+	private static void withdraw() {	// 출금하기
 		System.out.println("---");
 		System.out.println("출금");
 		System.out.println("---");
 		System.out.print("계좌번호: ");
 		String ano = scanner.next();
-		
 		System.out.print("출금액: ");
 		int money = scanner.nextInt();
 		
-	// 계좌 검색 메소드 호출
-		Ex20_Account account = findAccount(ano);
-		
-		if(findAccount(ano) == null) {
+		Ex20_Account account = findAccount(ano);			// 계좌 검색 메소드 호출
+		if (findAccount(ano) == null) {
 			System.out.println("결과: 찾는 계좌정보가 없습니다.");
 			return;
 		}
 		
-		//get으로 받아오는 금액에 입력한 money를 더해서 set에 넣음
-		account.setBalance(account.getBalance() - money);
+		account.setBalance(account.getBalance() - money);			// get으로 받아오는 금액에 입력한 money를 더해서 set에 넣음
 		System.out.println("결과 : 출금이 성공되었습니다.");
 	}
-	
+
 	private static void delete() {
 		System.out.println("------");
 		System.out.println("계좌삭제");
 		System.out.println("------");
 		System.out.print("계좌번호: ");
 		String ano = scanner.next();
-		
+
 		Ex20_Account account = findAccount(ano);
-		
-		for(int i = 0; i < accountArray.length; i++) {
-			if(findAccount(ano).equals(accountArray[i])) {
-				accountArray[i] = null;
+		account = null;
+		for (int i = 0; i < accountArray.length; i++) {
+			if (findAccount(ano).equals(accountArray[i])) {
+				accountArray[i] = accountArray[i + 1];
+				accountArray[i + 1] = null;
 				System.out.println("계좌가 삭제되었습니다.");
 				break;
 			}
 		}
-		
+
 	}
-	
-	//계좌찾기
-	private static Ex20_Account findAccount(String ano) {
+
+	private static Ex20_Account findAccount(String ano) {		// 계좌찾기
 		Ex20_Account account = null;
-		for(int i = 0; i < accountArray.length; i++) {
-			if(accountArray[i] != null) {
-				if(accountArray[i].getAno().equals(ano)) {
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				if (accountArray[i].getAno().equals(ano)) {
 					account = accountArray[i];
 					break;
 				}
