1e6b64c6a3ad7cc40e8afb628e43acb96e307e34
Jeonhyeunmin <jhm714@naver.com>
2024-09-10 02:21:23 +0900

    20240910 02:20

--- src/t50_ex/T5_indexOf.java
+++ src/t50_ex/T5_indexOf.java
@@ -1,5 +1,7 @@
 package t50_ex;
 
+import java.util.Scanner;
+
 /*
 	단어(문장)을 입력받은 후, 입력받은 글자 중에서 특정 문자가 어디 / 어디에 위치하고 있는지를 출력하시오.
 	예) 문장 : Welcome to Korea!
@@ -9,19 +11,27 @@ package t50_ex;
 */
 public class T5_indexOf {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		String index = "";
 		int cnt = 0;
-		String msg = "Welcome to Korea!";
+		
+		System.out.println("문장을 입력하세요.");
+		String msg = sc.nextLine();
+		System.out.println("찾을 문자를 입력하세요.");
+		char find = sc.next().charAt(0);
+		
 		char msgArr[] = new char[msg.length()];
 		msg.getChars(0, msg.length(), msgArr, 0);
 		
 		for(int i = 0 ; i < msgArr.length; i++) {
-			if(msgArr[i] == 'o') {
+			if(find == msgArr[i]) {
 				index += i + " / ";
 				cnt++;
 			}
 		}
-		System.out.println("'o'을 포함하고 있는 갯수 : " + cnt + "개");
-		System.out.println("'o'가 위치한 곳 : " + index.substring(0,index.length()-2));
+		System.out.println(find + "를 포함하고 있는 갯수 : " + cnt + "개");
+		System.out.println(find + "가 위치한 곳 : " + index.substring(0,index.length()-2));
+		System.out.println();
+		sc.close();
 	}
 }
